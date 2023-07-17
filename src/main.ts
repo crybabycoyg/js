079e4ec592a0f18a5832aa4e7cd1f6a5e0a62dbf
Mark Shuster <gunners2013@yandex.ru>
2023-07-17 20:42:15 +0300

    das

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import './style.css'
 
-let input=parseInt(prompt('Введите число', '0') as string)
-alert('Vi vveli ${input}, vo vtoroy stepeni eto ${input*input}')
+let input = parseInt(prompt('Введите число', '0') as string)
+alert(`U pass ${input}, in second degree it's`) 
