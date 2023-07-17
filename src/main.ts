5eb12abaa55dbff758314794dde01519944ebdb5
Mark Shuster <gunners2013@yandex.ru>
2023-07-17 20:54:25 +0300

    dasd

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import './style.css'
 
-let input = parseInt(prompt('Введите число', '10') as string)
-alert(`U pass ${input}, in second degree it's 100`) 
+// let input = parseInt(prompt('Введите число', '10') as string)
+// alert(`U pass ${input}, in second degree it's 100`) 
+
+
+
+let name = prompt('Как вас зовут', '');
+
+alert('Ваше имя ' + name)
