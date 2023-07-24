4043786482148e8e3b9f853837e37c66da873bfc
Mark Shuster <gunners2013@yandex.ru>
2023-07-24 20:14:48 +0300

    sads

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,12 @@ import './style.css'
 //   alert('Не знаете? ECMAScript!');
 // }
 
-let message = (login == 'Сотрудник') ? 'Привет' :
-  (login == 'Директор') ? 'Здравствуйте' :
-  (login == '') ? 'Нет логина' :
-  '';
+let value = prompt('Введите число', 0);
+
+if (value > 0) {
+    alert(1);
+} else if (value < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
