5bfbed0aa3275222fff5afbe7ca8a0dfe24d268b
Mark Shuster <gunners2013@yandex.ru>
2023-07-24 20:42:12 +0300

    ddasd

--- src/main.ts
+++ src/main.ts
@@ -59,3 +59,21 @@ import './style.css'
 // }
 
 // alert(accessAllowed);
+
+
+
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Мало' );
+    break;
+  case 4:
+    alert( 'Молодец!' );
+    break;
+  case 5:
+    alert( 'Перебор' );
+    break;
+  default:
+    alert( "Нет таких значений" );
+}
