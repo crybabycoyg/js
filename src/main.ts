375a0de31988669e837a4f18692c575f168de272
Mark Shuster <gunners2013@yandex.ru>
2023-07-24 20:46:51 +0300

    ssas

--- src/main.ts
+++ src/main.ts
@@ -62,13 +62,13 @@ import './style.css'
 
 
 
-if (browser == 'Edge') {
+if(browser == 'Edge') {
     alert("You've got the Edge!");
-} else if (browser == 'Chrome'
-    || browser == 'Firefox'
-    || browser == 'Safari'
-    || browser == 'Opera') {
-    alert('Okay we support these browsers too');
-} else {
-    alert('We hope that this page looks ok!');
-}
+  } else if (browser == 'Chrome'
+   || browser == 'Firefox'
+   || browser == 'Safari'
+   || browser == 'Opera') {
+    alert( 'Okay we support these browsers too' );
+  } else {
+    alert( 'We hope that this page looks ok!' );
+  }
