1393d34b5ca94f65cbae004350b0cb9ccaff6c98
Mark Shuster <gunners2013@yandex.ru>
2023-07-24 19:42:02 +0300

    sdf

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,38 @@
 import './style.css'
 
-                // Степень       
+//                 // Степень       
 
-// let input = parseInt(prompt('Введите число', '10') as string)
-// alert(`U pass ${input}, in second degree it's 100`) 
-
-
-
-                // Имя
-
-// let name = prompt('Как вас зовут', '');
-
-// alert('Ваше имя ' + name)
+// // let input = parseInt(prompt('Введите число', '10') as string)
+// // alert(`U pass ${input}, in second degree it's 100`) 
 
 
-                    //    Степень
 
-// let input=parseFloat(prompt('Введите число') as string)
-// let input2=parseFloat(prompt('Введите степень') as string)
-// alert( input ** input2 );
+//                 // Имя
 
-                        //   Который час
+// // let name = prompt('Как вас зовут', '');
 
-let input=parseFloat(prompt('Текущий час') as string)
-let input2=parseFloat(prompt ('Текущие минуты') as string)
-let h=parseInt(`${(1440-input*60-input2)/60}`)
-alert(`до следующего дня осталось ${h} часов ${60-(input2)} минут`)
+// // alert('Ваше имя ' + name)
+
+
+//                     //    Степень
+
+// // let input=parseFloat(prompt('Введите число') as string)
+// // let input2=parseFloat(prompt('Введите степень') as string)
+// // alert( input ** input2 );
+
+//                         //   Который час
+
+// let input=parseFloat(prompt('Текущий час') as string)
+// let input2=parseFloat(prompt ('Текущие минуты') as string)
+// let h=parseInt(`${(1440-input*60-input2)/60}`)
+// alert(`до следующего дня осталось ${h} часов ${60-(input2)} минут`)
+
+
+
+let company = prompt('Какое официальное название JavaScript?', '');
+
+if (company == 'Netscape') {
+  alert('Верно!');
+} else {
+  alert('Не знаете? “ECMAScript”!');
+}
